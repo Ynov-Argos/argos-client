ef0be82ce2f397712370c118165f7d2e23f4822f
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2024-07-22 22:40:33 +0200

    feat(ARG-34): :construction: forms architecture

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DefaultLayout from '../../layout/DefaultLayout.tsx';
 import { useGetUserQuery } from '../../services/user/UserApiSlice.ts';
 import Loader from '../../common/Loader';
-
+import UserForm from '../../components/Forms/User/UserForm.tsx';
 
 const User: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const [isNewUser, setIsNewUser] = useState(true);
 
-  const {data: user, isLoading, isError, isSuccess, error} = useGetUserQuery(id);
+  const {
+    data: user,
+    isLoading,
+    isError,
+    isSuccess,
+    error
+  } = useGetUserQuery(id);
 
   useEffect(() => {
     if (isError) {
-      console.log(error);
+      // @ts-ignore
+      if (error.data.statusCode === 404) {
+        setIsNewUser(true);
+      }
     } else if (isSuccess) {
-      console.log(user);
+      setIsNewUser(false);
     }
   }, [isError, error, isSuccess, user]);
 
 
 
 
-  return isLoading ? (<Loader></Loader>) : (
-    <DefaultLayout>
-      <div>
-        <h1>Hello User n°{id}</h1>
-      </div>
-    </DefaultLayout>
-  );
+  return isLoading ? (<Loader></Loader>) : (<DefaultLayout>
+    <div>
+      <h1>Hello User n°{id}</h1>
+      <UserForm isNewUser={isNewUser}></UserForm>
+    </div>
+  </DefaultLayout>)
 }
 
 export default User;
