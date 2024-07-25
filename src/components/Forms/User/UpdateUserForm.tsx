5b9f98db4a3fbab83556b2ed9082529e088d9e12
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2024-07-25 19:58:21 +0200

    feat(ARG-36): :sparkles: handle error same email

--- src/components/Forms/User/UpdateUserForm.tsx
+++ src/components/Forms/User/UpdateUserForm.tsx
@@ -1,16 +1,33 @@
 import { useState } from 'react';
 import SelectGroupDropdown from '../SelectGroupDropdown.tsx';
+import { useUpdateUserMutation } from '../../../services/user/UserApiSlice.ts';
+import { toast, ToastContainer } from 'react-toastify';
 
 const updateUserForm = ({user}) => {
   const [name, setName] = useState(user?.name || '');
   const [email, setEmail] = useState(user?.email || '');
   const [role, setRole] = useState(user?.role || '');
 
+  const [updateUser] = useUpdateUserMutation();
+
   const roleOptions = [{value: 'ADMIN', label: 'Admin'}, {value: 'STAFF', label: 'Staff'}];
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(user);
-    console.warn('Submit');
+    console.log(user.id);
+    const userToUpdate = {userId: user.id, name, email, role};
+    try {
+      const data = await updateUser(userToUpdate).unwrap();
+      setName(data?.name);
+      setEmail(data?.email);
+      setRole(data?.role);
+    } catch (err) {
+      console.error(err);
+      if (err.data.statusCode === 404) {
+        toast.error('Utilisateur inexistant');
+      } else {
+        toast.error(`Erreur ${err.data.statusCode}: ${err.data.message}`);
+      }
+    }
   }
 
   return (
@@ -54,17 +71,6 @@ const updateUserForm = ({user}) => {
             </div>
 
             <div className="mb-5.5 flex flex-col gap-6 xl:flex-row">
-              <div className="w-full xl:w-1/2">
-                <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  placeholder="yourmail@gmail.com"
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 font-normal text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-
               <div className="mb-5.5">
                 <label
                   className="mb-3 block text-sm font-medium text-black dark:text-white"> Rôle </label>
@@ -80,6 +86,7 @@ const updateUserForm = ({user}) => {
           </div>
         </form>
       </div>
+      <ToastContainer/>
     </div>
   );
 };
